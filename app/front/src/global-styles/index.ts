f760aa387a60df4047dfeeeaefcfa12c241af33e
Irving Armenta <irving@wealthpark.loc>
2021-05-04 13:29:29 +0900

    style: added a simple box-shadow to all pixel-border elements

--- app/front/src/global-styles/index.ts
+++ app/front/src/global-styles/index.ts
@@ -49,6 +49,7 @@ export const GlobalStyles = createGlobalStyle`
     border-image-width: 2;
     border-image-outset: 0;
     border-image-source: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='12' height='12'><path d='M2 2h2v2H2zM4 0h2v2H4zM10 4h2v2h-2zM0 4h2v2H0zM6 0h2v2H6zM8 2h2v2H8zM8 8h2v2H8zM6 10h2v2H6zM0 6h2v2H0zM10 6h2v2h-2zM4 10h2v2H4zM2 8h2v2H2z' fill='%23000' /></svg>");
+    box-shadow: 0 0 8px #d9d9d9;
   }
 
   button {
